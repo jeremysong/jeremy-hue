aa3c19d7c74cafbada288a3f4f3d0bb270b4649b
Jeremy Song <jeremy.song@me.com>
2021-10-30 16:18:34 -0700

    Change let to const

--- app.js
+++ app.js
@@ -8,10 +8,11 @@ if (!state) {
 var Hue = require('philips-hue-api'),
     hue = Hue('http://192.168.4.35/api/nrY2S1lbZthmEJ3QwMfXuHpYzUC7wWrOk4Ex1dOL/');
 
-let lightNames = ["Kitchen 1", "Kitchen 2", "Task Lamp"]
+const LIGHT_NAMES = ["Kitchen 1", "Kitchen 2", "Task Lamp"]
 
 let controlLights = function(command, args) {
-    lightNames.forEach(name => {
+    LIGHT_NAMES
+.forEach(name => {
         hue.lights(name)[command](args);
     })
 }
