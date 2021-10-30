cb46f76442c3de55718363a0ae246a58e90ba349
Jeremy Song <jeremy.song@me.com>
2021-10-30 14:10:14 -0700

    Turn on and off three lights together

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
+let args = process.argv.slice(2);
+let state = args[0];
+if (!state) {
+    // State state to "on" if it's undefined.
+    state = 'on';
+}
+
 var Hue = require('philips-hue-api'),
     hue = Hue('http://192.168.4.35/api/nrY2S1lbZthmEJ3QwMfXuHpYzUC7wWrOk4Ex1dOL/');
 
-hue.lights(4).on();
+let lightNames = ["Kitchen 1", "Kitchen 2", "Task Lamp"]
 
-hue.lights().list(function (error, lights) {
-    console.log(lights);
-})
+lightNames.forEach(name => {
+    hue.lights(name)[state]();
+});
